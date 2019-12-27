6feb96fb55ef093b41c6c597a6792e6d4d326451
Ellis Min <dbmin@hotmail.com>
2019-12-27 05:47:54 -0600

    changed the file structure and added more routes & views & controllers  to expand app functionalities

---
+++ 08-MVC/controllers/shop.js
@@ -0,0 +1,37 @@
+const Product = require("../models/product");
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Home",
+      path: "/"
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/products"
+    });
+  });
+};
+
+
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart"
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("/shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout"
+  });
+};
