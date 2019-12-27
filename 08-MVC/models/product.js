fbf770f55d90e851ff423100ea851e18341d6945
Ellis Min <dbmin@hotmail.com>
2019-12-27 05:03:26 -0600

    removed duplicated codes in product model

--- 08-MVC/models/product.js
+++ 08-MVC/models/product.js
@@ -2,39 +2,38 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
 
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      // Read the file & put into products if exists
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(rootDir, "data", "products.json");
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-
-      // Read the file & put into products if exists
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile(products => {
       // Write the product into file
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
+
+    fs.readFile(p, (err, fileContent) => {});
   }
 
   // static allows to call this function on Product itself
   static fetchAll(cb) {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(fileContent));
-      }
-    });
+    getProductsFromFile(cb);
   }
 };
