676c48ac179f9ca9d5d0308d125def3540e07338
Ellis Min <dbmin@hotmail.com>
2019-12-27 02:10:45 -0600

    added codes for ejs template engine

--- 07-dynamic-content/app.js
+++ 07-dynamic-content/app.js
@@ -7,20 +7,21 @@ const path = require("path");
 const app = express();
 
 // To use Handlebar
-const expressHbs = require("express-handlebars");
-
-app.engine(
-  "hbs",
-  expressHbs({
-    // layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-); // first param needs to match with file extension & app.set( , *) below
+// const expressHbs = require("express-handlebars");
+// // first param needs to match with file extension & app.set( , *) below
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     // layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs"
+//   })
+// );
 
 // Setting global config; view engine to set templating engine
 // app.set("view engine", "pug"); // looks for 2nd param extensions
-app.set("view engine", "hbs"); // looks for 2nd param extensions
+// app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "07-dynamic-content/views"); // second param sets directory from root directory
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,10 +36,10 @@ app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 
   // Using templating engine
-  // res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 
   // For hbs
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  // res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
