537cccd7da68d02113e3de32f82af63e08254d50
Ellis Min <dbmin@hotmail.com>
2019-12-27 04:52:58 -0600

    On adding a product, it saves into JSON file and displays products from JSON file on getProducts

--- 08-MVC/models/product.js
+++ 08-MVC/models/product.js
@@ -1,5 +1,6 @@
-
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
 
 module.exports = class Product {
   constructor(title) {
@@ -7,11 +8,33 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, "data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      // Read the file & put into products if exists
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      // Write the product into file
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
   // static allows to call this function on Product itself
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      } else {
+        cb(JSON.parse(fileContent));
+      }
+    });
   }
 };
