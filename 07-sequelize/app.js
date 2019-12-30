be48c4d40ebdb839970e4ee95fdcc7cf319a4e11
Ellis Min <dbmin@hotmail.com>
2019-12-29 20:03:52 -0600

    created a dummy user

--- 07-sequelize/app.js
+++ 07-sequelize/app.js
@@ -30,12 +30,33 @@ app.use(errorController.getPageNotFound);
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product); // Optional but is to make it more clear
 
+// Giving app access to user
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      // Adding a new field to req obj; stores sequelize obj
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 // CREATE TABLE that's stored in SQL server
 sequelize
-  // force overwrites table
-  .sync({ force: true })
+  .sync()
   .then(result => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then(user => {
+    // Creating dummy user
+    if (!user) {
+      return User.create({ name: "Ellis", email: "test@test.test" });
+    }
+    return Promise.resolve(user); // Promise.resolve() is optional
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
