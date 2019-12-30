c83d8dfce87bfe7fcd5a59b017f358c486ef6203
Ellis Min <dbmin@hotmail.com>
2019-12-30 07:05:08 -0600

    displays cart items with getCart()--returns all products in cart

--- 08-mongodb/controllers/shop.js
+++ 08-mongodb/controllers/shop.js
@@ -43,18 +43,12 @@ exports.getProduct = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      // getProducts() possible through sequelize
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render("shop/cart", {
-            path: "/cart",
-            pageTitle: "Your Cart",
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -68,43 +62,11 @@ exports.postCart = (req, res, next) => {
     })
     .then(result => {
       // console.log(result);
+      res.redirect("/cart");
     })
     .catch(err => {
       console.log(err);
     });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     // Check if product exists in cart
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     // If product exists, increase quantity
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then(product => {
-  //     // addProduct(): method added by sequelize for many to many relationship
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
