5f0975ee6aa69ddb50c5bf529dd6dd9f9224e181
Ellis Min <dbmin@hotmail.com>
2019-12-30 17:10:12 -0600

    created mongoose db connection. Commented out moroutes and models to integrate them with mongoose

--- 09-mongoose/app.js
+++ 09-mongoose/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const errorController = require("./controllers/error");
-
-const mongoConnect = require("./util/db").mongoConnect;
+const mongoose = require("mongoose");
 const User = require("./models/user");
 
 const app = express();
@@ -18,15 +17,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.getById("5e09e336798a43d47c624059")
-    .then(user => {
-      // Adding a new field to req obj; to be used elsewhere
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.getById("5e09e336798a43d47c624059")
+//     .then(user => {
+//       // Adding a new field to req obj; to be used elsewhere
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 // Handle routes
 app.use("/admin", adminRoutes);
@@ -34,11 +33,16 @@ app.use(shopRoutes);
 app.use(errorController.getPageNotFound);
 
 // db connection
-mongoConnect(() => {
-  // Set prot to whatever is in the environment variable PORT,
-  // or 3000 if there's nothing there.
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+mongoose
+  .connect("mongodb+srv://Ellis:00000000@cluster0-6s9e0.mongodb.net/test", {
+    useNewUrlParser: true
+  })
+  .then(result => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
--- 09-mongoose/routes/admin.js
+++ 09-mongoose/routes/admin.js
@@ -2,23 +2,23 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
 
-// /admin/add-product => GET
-router.get("/add-product", adminController.getAddProduct);
+// // /admin/add-product => GET
+// router.get("/add-product", adminController.getAddProduct);
 
-// /admin/add-product => POST
-router.post("/add-product", adminController.postAddProduct);
+// // /admin/add-product => POST
+// router.post("/add-product", adminController.postAddProduct);
 
-// /admin/product-list => GET
-router.get("/product-list", adminController.getProducts);
+// // /admin/product-list => GET
+// router.get("/product-list", adminController.getProducts);
 
 
-// /admin/edit-product/[id] => GET
-router.get("/edit-product/:productId", adminController.getEditProduct);
+// // /admin/edit-product/[id] => GET
+// router.get("/edit-product/:productId", adminController.getEditProduct);
 
-// /admin/edit-product/[id] => POST
-router.post("/edit-product", adminController.postEditProduct);
+// // /admin/edit-product/[id] => POST
+// router.post("/edit-product", adminController.postEditProduct);
 
-// /admin/delete/[id] => POST
-router.post("/delete-product/:productId", adminController.postDeleteProduct);
+// // /admin/delete/[id] => POST
+// router.post("/delete-product/:productId", adminController.postDeleteProduct);
 
 module.exports = router;
