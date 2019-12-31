f1e2a329b7303d209a668145424659e56ba72861
Ellis Min <dbmin@hotmail.com>
2019-12-31 01:33:01 -0600

    sends cookie to the browser on logging in, then displays admin menu

--- 10-sessions-cookies/controllers/admin.js
+++ 10-sessions-cookies/controllers/admin.js
@@ -4,7 +4,8 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false
+    editing: false,
+    isAuthenticated: req.isLoggedIn
   });
 };
 
@@ -19,7 +20,8 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     // equivalent to user._id; mongoose automatically picks the id form obj
-    userId: req.user
+    userId: req.user,
+    isAuthenticated: req.isLoggedIn
   });
 
   product
@@ -50,7 +52,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product
+        product: product,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -72,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
       product.description = updatedDesc;
       product.price = updatedPrice;
       product.imgUrl = updatedImgUrl;
-      
+
       // save() automatically updates in mongoose with updated fields above
       return product.save();
     })
--- 10-sessions-cookies/controllers/auth.js
+++ 10-sessions-cookies/controllers/auth.js
@@ -1,6 +1,19 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split("=")[1]; // set to true
   res.render("auth/login", {
     path: "/login",
-    pageTitle: "Login"
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  // this data gets lost after response is finished
+  // req.isLoggedIn = true;
+
+  // Sends cookie that gets stored in the browser;
+  // visible in Application->Cookies tab or Network-> page's header
+  // expiration: session or 1969/../.. that expires on closing browser
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
