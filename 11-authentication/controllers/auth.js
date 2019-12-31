cb1a3675d12f4862ad11992d5a31bbdd3316f981
Ellis Min <dbmin@hotmail.com>
2019-12-31 06:22:23 -0600

    encrypt password when storing in db with bcrypt

--- 11-authentication/controllers/auth.js
+++ 11-authentication/controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
@@ -51,16 +52,21 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect("/signup");
       }
-      const user = new User({
-        email: email,
-        password: password,
-        cart: { items: [] }
-      });
-      return user.save();
-    })
-    .then(result => {
-      console.log("User created");
-      res.redirect("/login");
+      // encrypt with 12 rounds of hashing
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          console.log("User created");
+          res.redirect("/login");
+        });
     })
     .catch(err => {
       console.log(err);
