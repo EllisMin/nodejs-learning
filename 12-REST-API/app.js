f3872593e18686c1e694e0f6843f079474dda755
Ellis Min <dbmin@hotmail.com>
2020-01-08 21:12:14 -0600

    created basic REST API that interacts with client that fetches json data

--- 12-REST-API/app.js
+++ 12-REST-API/app.js
@@ -1,7 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const feedRoutes = require("./routes/feed");
+
 const app = express();
+
+// app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+// Set headers to prevent CORS Error
+app.use((req, res, next) => {
+  // 1st: header type, 2nd: which domain to allow
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Allow specific methods
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+
+  // Allows client to set headers with Content-Type
+  res.setHeader("Access-Control-Allow-Headers",
+   "Content-Type, Authorization");
+  next();
+});
 
 app.use("/feed", feedRoutes);
 
--- 12-REST-API/controllers/feed.js
+++ 12-REST-API/controllers/feed.js
@@ -1,3 +1,20 @@
 exports.getPosts = (req, res, next) => {
-    
-}
+  // Status code 200 is default
+  res.status(200).json({
+    posts: [{ title: "First Post",
+    content: "This is the first post!" }]
+  });
+};
+
+exports.postPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  console.log(title, content); ///
+
+  // Create pst in db; Status 201 is to tell resource is created, successfully
+  res.status(201).json({
+    message: "Post created",
+    post: { id: new Date().toISOString(),
+    title: title, content: content }
+  });
+};
