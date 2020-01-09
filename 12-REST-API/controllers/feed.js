9e82e3c5d2b622d0024c71f189fa7ba00e0adbd1
Ellis Min <dbmin@hotmail.com>
2020-01-09 13:44:47 -0600

    added end point and validation for posting a post

--- 12-REST-API/controllers/feed.js
+++ 12-REST-API/controllers/feed.js
@@ -1,20 +1,47 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   // Status code 200 is default
   res.status(200).json({
-    posts: [{ title: "First Post",
-    content: "This is the first post!" }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imgUrl: "12-REST-API/images/pen.jpeg",
+        creator: {
+          name: "Ellis"
+        },
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.postPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is incorrect",
+        errors: errors.array()
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title, content); ///
 
   // Create pst in db; Status 201 is to tell resource is created, successfully
   res.status(201).json({
     message: "Post created",
-    post: { id: new Date().toISOString(),
-    title: title, content: content }
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "Elis"
+      },
+      createdAt: new Date()
+    }
   });
 };
