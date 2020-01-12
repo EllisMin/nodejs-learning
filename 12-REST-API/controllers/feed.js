c5e228211a1fe024f911065b7334b3deb683eac8
Ellis <dbmin@hotmail.com>
2020-01-11 22:36:48 -0600

    handles updating a post

--- 12-REST-API/controllers/feed.js
+++ 12-REST-API/controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
@@ -77,4 +79,60 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imgUrl = req.body.image;
+  if (req.file) {
+    imgUrl = req.file.path;
+  }
+  if (!imgUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+  
+  // Update in db
+  Post.findById(postId)
+    .then(post => {
+      
+      if (!post) {
+        const error = new Error("Could not find the post");
+        error.statusCode = 404;
+        // Passed down to catch()
+        throw error;
+      }
+      if (imgUrl !== post.imgUrl) {
+        clearImage(post.imgUrl);
+      }
+      post.title = title;
+      post.imgUrl = imgUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then(result => {
+      // Use 201 for adding a new resource
+      res.status(200).json({ message: "Post updated", post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "../..", filePath);
+  // Delete file
+  fs.unlink(filePath, err => console.log(err));
 };
--- 12-REST-API/routes/feed.js
+++ 12-REST-API/routes/feed.js
@@ -24,4 +24,18 @@ router.post(
 
 router.get("/post/:postId", feedController.getPost);
 
+// Http method that overwrites existing item
+router.put(
+  "/post/:postId",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 5 }),
+    body("content")
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
 module.exports = router;
