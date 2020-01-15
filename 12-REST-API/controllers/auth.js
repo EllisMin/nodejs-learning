fe318767d307033e180ae37f7cbd23b33f8f1b57
Ellis Min <dbmin@hotmail.com>
2020-01-14 21:36:14 -0600

    authenticates user when signing in with json web token generated from server and saved in local storage

--- 12-REST-API/controllers/auth.js
+++ 12-REST-API/controllers/auth.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
@@ -27,6 +29,46 @@ exports.signup = (req, res, next) => {
     .then(result => {
       // 201: resource created
       res.status(201).json({ message: "User created", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error("A user with this email couldn't be found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401;
+        throw error;
+      }
+      // Configure token for authentication
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        "someSecretStringValue!@",
+        // Logs out in an hour
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
