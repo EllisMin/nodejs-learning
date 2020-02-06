6b95c71218de1341f80d57c885633e102610c420
Ellis Min <dbmin@hotmail.com>
2020-02-06 02:01:33 -0600

    [TEST & REFACTOR]: creates db connection in the begninng. Removes dummy user and disconnects db after all test cases

--- 12-REST-API/test/auth-controller.js
+++ 12-REST-API/test/auth-controller.js
@@ -8,6 +8,38 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function() {
+  // Runs before all test cases
+  before(function(done) {
+    // db connection-- DO NOT USE Production db
+    mongoose
+      .connect(process.env.MONGODB_URI_MESSAGES_TEST, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      })
+      .then(result => {
+        // Dummy user
+        const user = new User({
+          _id: "5e396f9615f4e09d13540703",
+          email: "test@a.com",
+          password: "1234",
+          name: "testname",
+          posts: []
+        });
+        // Save test user
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+  
+  // Runs before EVERY test case--may be useful to initialize
+  // beforeEach(function() {})
+  // Runs after EVERY test case
+  // afterEach(function() {})
+
+  // Use function(done) for use of async fun
   it("should throw an error with status code 500 when fails to access db", function(done) {
     // Testing User.findOne()
     sinon.stub(User, "findOne");
@@ -35,49 +67,34 @@ describe("Auth Controller - Login", function() {
 
   // Testing db
   it("should send a response with a valid user status for an existing user", function(done) {
-    // db connection-- DO NOT USE Production db
+    const req = { userId: "5e396f9615f4e09d13540703" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        this.userStatus = data.status;
+      }
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new");
+      done();
+    });
+  });
 
-    // console.log(process.env.MONGODB_URI_MESSAGES_TEST); ///
-
-    mongoose
-      .connect(process.env.MONGODB_URI_MESSAGES_TEST, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-      })
-      .then(result => {
-        // Dummy user
-        const user = new User({
-          _id: "5e396f9615f4e09d13540703",
-          email: "test@a.com",
-          password: "1234",
-          name: "testname",
-          posts: []
-        });
-        // Save test user
-        return user.save();
+  after(function(done) {
+    // Remove dummy users in db
+    User.deleteMany({})
+      .then(() => {
+        // Disconnect db connection
+        return mongoose.disconnect();
       })
       .then(() => {
-        const req = { userId: "5e396f9615f4e09d13540703" };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function(code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function(data) {
-            this.userStatus = data.status;
-          }
-        };
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal("I am new");
-          done();
-        });
-      })
-      .catch(err => {
-        console.log(err);
+        done();
       });
   });
 });
